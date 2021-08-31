416ef68d8e2e9914c04c318614015b3c9024a4b4
unknown <nuntawat222542@gmail.com>
2021-08-31 17:06:01 +0700

    function rejs

--- start.js
+++ start.js
@@ -1,6 +1,13 @@
 (() =>{
 
 function simulateAsyncAPI(text, timeout){
+        return new Promise ((resolve, reject)=>{
+            setTimeout(()=>{
+                // if (text=== 'B') return reject ('B got rejected');
+                console.log(text);
+                resolve();
+            },timeout);
+        });
     }
     async function run(){
     }
